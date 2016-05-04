77b77eef147e98096d78237e05279dbdf0e90466
Tom W Hartung <tomwhartung@gmail.com>
2016-05-04 18:16:59 +0000

    Adding a version of the blink example written in python, from the Make: Getting Started With Intel Edison book.

---
+++ my_examples/01.Basics/blink.py
@@ -0,0 +1,16 @@
+
+import mraa
+import time
+
+ledPin = 13
+sleepTime = 0.2   # blink timing
+
+x = mraa.Gpio(ledPin)
+x.dir(mraa.DIR_OUT)
+
+while True:
+    x.write(1)
+    time.sleep(sleepTime)
+    x.write(0)
+    time.sleep(sleepTime)
+
