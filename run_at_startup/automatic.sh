79524f6b082686e8f0993053d963fc0993ef2bcb
Tom W Hartung <tomwhartung@gmail.com>
2016-05-04 21:07:06 +0000

    Added comments detailing how to make this script run when the edison boots up.

--- run_at_startup/automatic.sh
+++ run_at_startup/automatic.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 #
+# To run the script referenced below at startup:
+# (1) copy this script (automatic.sh) into /etc/init.d (as root)
+# (2) run this command (as root):
+#     update-rc.d automatic.sh 
+#
 python /home/root/iot/customizations/edison_python/my_examples/01.Basics/blink.py
 
