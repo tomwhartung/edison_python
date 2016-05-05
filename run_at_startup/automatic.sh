4e3cdf8cb5b2e1219421ec7640a99fc79a53851c
Tom W Hartung <tomwhartung@gmail.com>
2016-05-05 04:33:49 +0000

    Updated a comment with the correct command (important!).

--- run_at_startup/automatic.sh
+++ run_at_startup/automatic.sh
@@ -3,7 +3,7 @@
 # To run the script referenced below at startup:
 # (1) copy this script (automatic.sh) into /etc/init.d (as root)
 # (2) run this command (as root):
-#     update-rc.d automatic.sh 
+#     update-rc.d automatic.sh defaults
 #
 if [ -f ~/iot/flags/run_python_on_startup.flag ]; then
 	python /home/root/iot/customizations/edison_python/my_examples/01.Basics/blink.py
