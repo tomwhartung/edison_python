ab6076e31c059d94bd5a9f254f579d6cf7f66ac0
Tom W Hartung <tomwhartung@gmail.com>
2016-05-05 17:02:57 +0000

    Updated to blink the letter "P" (for Python) in Morse Code.  This version is untested, I need to reboot before testing (running the Arduino sketch) and it seems prudent to check in these changes before doing that....

--- my_examples/01.Basics/blink.py
+++ my_examples/01.Basics/blink.py
@@ -3,14 +3,37 @@ import mraa
 import time
 
 ledPin = 13
-sleepTime = 0.2   # blink timing
+dotMs  = 200
+dashMs = 3 * dotMs
+elementGapMs = dotMs
+letterGapMs = dashMs
 
 x = mraa.Gpio(ledPin)
 x.dir(mraa.DIR_OUT)
 
-while True:
+//
+// dot: turn led on for dot milliseconds, then turn it off
+//
+def dot():
     x.write(1)
-    time.sleep(sleepTime)
+    time.sleep(dotMs)
     x.write(0)
-    time.sleep(sleepTime)
+//
+// dash: turn led on for dash milliseconds, then turn it off
+//
+def dash():
+    x.write(1)
+    time.sleep(dashMs)
+    x.write(0)
+
+//
+// mainline loop: write a "P" (for python) in Morse Code "forever"
+// A "P" in Morse Code is dot-dash-dash-dot
+// Morse Code Reference: https://en.wikipedia.org/wiki/Morse_code#Representation.2C_timing_and_speeds
+//
+while True:
+    dot()
+    time.sleep(elementGapMs)
+    dash()
+    time.sleep(letterGapMs)
 
