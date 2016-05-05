a684cb97f98c77e50a50ecf9f68366f5415fa95b
Tom W Hartung <tomwhartung@gmail.com>
2016-05-05 17:18:03 +0000

    Fixed syntax errors.  Hey I remembered to use the colons and no curly braces but forgot to use the hash tag for comments.

--- my_examples/01.Basics/blink.py
+++ my_examples/01.Basics/blink.py
@@ -11,26 +11,26 @@ letterGapMs = dashMs
 x = mraa.Gpio(ledPin)
 x.dir(mraa.DIR_OUT)
 
-//
-// dot: turn led on for dot milliseconds, then turn it off
-//
+#
+# dot: turn led on for dot milliseconds, then turn it off
+#
 def dot():
     x.write(1)
     time.sleep(dotMs)
     x.write(0)
-//
-// dash: turn led on for dash milliseconds, then turn it off
-//
+#
+# dash: turn led on for dash milliseconds, then turn it off
+#
 def dash():
     x.write(1)
     time.sleep(dashMs)
     x.write(0)
 
-//
-// mainline loop: write a "P" (for python) in Morse Code "forever"
-// A "P" in Morse Code is dot-dash-dash-dot
-// Morse Code Reference: https://en.wikipedia.org/wiki/Morse_code#Representation.2C_timing_and_speeds
-//
+#
+# mainline loop: write a "P" (for python) in Morse Code "forever"
+# A "P" in Morse Code is dot-dash-dash-dot
+# Morse Code Reference: https://en.wikipedia.org/wiki/Morse_code#Representation.2C_timing_and_speeds
+#
 while True:
     dot()
     time.sleep(elementGapMs)
