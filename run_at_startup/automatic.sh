e78a2d29ea278c3087e6138d7e3a4c70dde36269
Tom W Hartung <tomwhartung@gmail.com>
2016-05-05 17:04:15 +0000

    Updated to run the SPP-loopback.py program on startup.

--- run_at_startup/automatic.sh
+++ run_at_startup/automatic.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
-# To run the script referenced below at startup:
+# To run this script at startup:
 # (1) copy this script (automatic.sh) into /etc/init.d (as root)
 # (2) run this command (as root):
 #     update-rc.d automatic.sh defaults
 #
+python /home/root/iot/customizations/edison_python/bluetooth/SPP-loopback.py &
+
 if [ -f ~/iot/flags/run_python_on_startup.flag ]; then
 	python /home/root/iot/customizations/edison_python/my_examples/01.Basics/blink.py
 fi
