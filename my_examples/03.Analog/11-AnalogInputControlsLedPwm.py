db03ae860432bcca2b59264b59921493d333a099
Tom W Hartung <tomwhartung@gmail.com>
2016-05-12 20:11:31 +0000

    Adding 11-AnalogInputControlsLedPwm.py which does the same thing as the arduino sketch in ex-10-AnalogInputControlsLeds, but uses PWM instead of changing the length of the delay.  Oh wait....

---
+++ my_examples/03.Analog/11-AnalogInputControlsLedPwm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+# 11-AnalogInputControlsLedPwm.py: Use the analog input value to control the brightness of the PWM led
+#
+import mraa
+import time
+
+analogInPin = 0
+analogInAio = mraa.Aio( analogInPin )
+
+#
+# NOTE: According to our "Make..." book (p.75), the only pins
+#    we can use for PWM are digital pins 3, 5, 6, and 9 
+#
+ledOutPin = 3
+ledOutPwm = mraa.Pwm( ledOutPin )
+ledOutPwm.enable( True )
+
+###################################
+# Functions for doin the stuffs
+#
+
+##
+# loop: what to do "forever"
+#
+def loop() :
+	analogInInteger = analogInAio.read()
+	analogInFloat = analogInAio.readFloat()
+	print( 'analogInInteger: ' + str(analogInInteger) + '; analogInFloat: ' + str(analogInFloat) )
+	ledOutPwm .write( analogInFloat )
+
+#
+# mainline loop:
+#
+readingGapSecs = 0.5
+while True:
+	loop()
+	time.sleep( readingGapSecs )
+
+exit(0)
