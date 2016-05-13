d3dcf9ea41aadc47a7b62c16a0875c39ac0cdc6f
Tom W Hartung <tomwhartung@gmail.com>
2016-05-13 16:24:49 +0000

    Removed the loop function (because it is kind of silly) and now it works better. but not quite well enough to move on to the next one.

--- my_examples/02.Digital/07-buttonTogglesLed.py
+++ my_examples/02.Digital/07-buttonTogglesLed.py
@@ -16,34 +16,31 @@ ledOutPin = 3
 ledOutGpio = mraa.Gpio( ledOutPin )
 ledOutGpio.dir(mraa.DIR_OUT)
 
-ledOutState = LOW
-
 #############################
 # Functions
 #
 def toggleLedState( ledOutState ) :
+	print ( 'toggleLedState ledOutState = ' + str(ledOutState) )
 	if ( ledOutState == LOW ) :
 		 ledOutState = HIGH
 	else :
 		 ledOutState = LOW
+	print ( 'toggleLedState returning ledOutState = ' + str(ledOutState) )
 	return ledOutState
-
-##
-# loop: what to do "forever"
-#
-def loop() :
-	ledOutGpio.write( ledOutState )
-	digitalInState = digitalInGpio.read()
-	print( 'digitalInState: ' + str(digitalInState) )
-	if( digitalInInteger == 1 ) :
-		ledOutState = toggleLedState( ledOutState )
 
 #
 # mainline loop:
 #
+ledOutState = LOW
 loopDelaySecs = 0.1
+
 while True:
-	loop()
+	ledOutGpio.write( ledOutState )
+	digitalInState = digitalInGpio.read()
+	print( 'digitalInState: ' + str(digitalInState) )
+	if( digitalInState == 1 ) :
+		ledOutState = toggleLedState( ledOutState )
+
 	time.sleep( loopDelaySecs )
 
 exit(0)
