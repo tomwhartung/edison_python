08e4f664d676fd0709353e81c07d307fb6f817b6
Tom W Hartung <tomwhartung@gmail.com>
2016-05-14 18:24:51 -0600

    First pass at major rewrite of 06-randomRgb.py in an effort to make it more fun.

--- my_examples/02.Digital/06-randomRgb.py
+++ my_examples/02.Digital/06-randomRgb.py
@@ -3,6 +3,7 @@
 # 06-randomRgb.py: toggle the leds we are using, each with a random and different sleep time
 # ------------------------------------------------------------------------------------------
 #
+from datetime import *
 import math, random
 import mraa
 import time
@@ -18,18 +19,49 @@ ledGpio2 = mraa.Gpio( ledPin2 )
 ledGpio3 = mraa.Gpio( ledPin3 )
 ledGpio4 = mraa.Gpio( ledPin4 )
 
-led2Secs = 0
-led3Secs = 0
-led4Secs = 0
-darkSecs = 0
+aed2State = HIGH
+led3State = HIGH
+led4State = HIGH
+
+maxCycleSecs = 5.0
+led2CycleSecs = 0
+led3CycleSecs = 0
+led4CycleSecs = 0
 
 ##
 # Functions
 #
-def getRandomSecs() :
-	maxSecs = 5.0
-	randomSecs = maxSecs * random.random()
-	return randomSecs
+def getRandomCycleSecs() :
+	randomCycleSecs = maxCycleSecs * random.random()
+	return randomCycleSecs
+
+##
+# Determine whether it is time to change the state of an led
+#
+def isTimeToToggle( cycleStartDatetime, cycleSecs ) :
+	cycleStartSecsOnly = getAttr( cycleStartDatetime, 'second' )
+	cycleStartMicrosecs = getAttr( cycleStartDatetime, 'microsecond' )
+	cycleStartSecs = cycleStartSecsOnly + ( cycleStartMicrosecs / 1000000 )
+	currentDatetime = datetime.today()
+	currentSecsOnly = getAttr( currentDatetime, 'second' )
+	currentMicrosecs = getAttr( currentDatetime, 'microsecond' )
+	currentSecs = currentSecsOnly + ( currentMicrosecs / 1000000 )
+	elapsedSecs = currentSecs - cycleStartSecs
+	print( 'currentSecs - cycleStartSecs = ' + str(currentSecs) + ' - ' + str(cycleStartSecs) + ' = ' + elapsedSecs )
+	if ( cycleSecs < elapsedSecs ) :
+		return True
+	else :
+		return False
+
+##
+# Return the opposite of the state passed in
+# This may seem unnecessary, but I just do not like statements of the form "state = !state"
+#
+def toggleState( currentState )
+	if ( currentState == HIGH ) :
+		return LOW
+	else :
+		return HIGH
 
 ##
 # setup: initialization
@@ -38,32 +70,38 @@ def setup() :
 	ledGpio2.dir(mraa.DIR_OUT)
 	ledGpio3.dir(mraa.DIR_OUT)
 	ledGpio4.dir(mraa.DIR_OUT)
-	print( 'led2Secs: ' + str(led2Secs) )
-	print( 'led3Secs: ' + str(led3Secs) )
-	print( 'led4Secs: ' + str(led4Secs) )
+	print( 'led2CycleSecs: ' + str(led2CycleSecs) )
+	print( 'led3CycleSecs: ' + str(led3CycleSecs) )
+	print( 'led4CycleSecs: ' + str(led4CycleSecs) )
 
 ##
 # loop: what to do "forever"
 #
 def loop() :
-	ledGpio2.write( HIGH )
-	time.sleep( led2Secs )
-	ledGpio2.write( LOW )
-	ledGpio3.write( HIGH )
-	time.sleep( led3Secs )
-	ledGpio3.write( LOW )
-	ledGpio4.write( HIGH )
-	time.sleep( led4Secs )
-	ledGpio4.write( LOW )
-	time.sleep( darkSecs )
+	if ( isTimeToToggle( led2Datetime, led2CycleSecs )  ):
+		led2State = toggleState( led2State )
+		ledGpio2.write( led2State )
+	if ( isTimeToToggle( )  ):
+		led3State = toggleState( led3State )
+		ledGpio3.write( led3State )
+	if ( isTimeToToggle( )  ):
+		led4State = toggleState( led4State )
+		ledGpio4.write( led4State )
 
 #
 # mainline code: in this case we do not loop, but just turn it off and exit
 #
-led2Secs = getRandomSecs()
-led3Secs = getRandomSecs()
-led4Secs = getRandomSecs()
-darkSecs = getRandomSecs()
+led2CycleSecs = getRandomCycleSecs()
+led3CycleSecs = getRandomCycleSecs()
+led4CycleSecs = getRandomCycleSecs()
+
+led2Datetime = datetime.today()
+led3Datetime = datetime.today()
+led4Datetime = datetime.today()
+
+ledGpio2.write( led2State )
+ledGpio3.write( led3State )
+ledGpio4.write( led4State )
 
 setup()
 
