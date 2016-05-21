844460e85b184a22aa1e6e55813b8f24beeb063d
Tom W Hartung <tomwhartung@gmail.com>
2016-05-21 10:58:16 -0600

    Adding 10-UsingPwmToControlLed.py , the python version of the similarly named arduino program.

---
+++ my_examples/03.Analog/10-UsingPwmToControlLed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#
+# 10-UsingPwmToControlLed.py: Use PWM to control the brightness of a PWM led
+#
+import mraa
+import time
+
+#
+# NOTE: the only pins we can use for PWM are digital pins 3, 5, 6, and 9 
+#
+ledOutPin = 3
+ledOutPwm = mraa.Pwm( ledOutPin )
+ledOutPwm.enable( True )
+
+##
+# loop: what to do "forever"
+#
+def loop( brightnessPercent ) :
+	print( 'brightnessPercent: ' + str(brightnessPercent) )
+	brightnessValue = brightnessPercent / 100
+	ledOutPwm.write( brightnessPercent )
+
+#
+# mainline: calls loop repeatedly
+#
+loopSleepSecs = 0.5
+brightnessPercent = 0
+deltaPercent = 5
+while True:
+	loop( brightnessPercent )
+	if( brightnessPercent <= 0 || brightnessPercent >= 100 ) :
+		deltaPercent = - deltaPercent
+	brightnessPercent = brightnessPercent + deltaPercent
+	time.sleep( loopSleepSecs )
+
+exit(0)
