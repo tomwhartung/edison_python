3955f67fd45f3c63b30e6d939bc62a46851e371e
Tom W Hartung <tomwhartung@gmail.com>
2016-05-22 04:14:42 +0000

    Adding 09-debouncedButtonTogglesLed.py ; initial version is very much like 07-buttonTogglesLed.py .

---
+++ my_examples/02.Digital/09-debouncedButtonTogglesLed.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#
+# 07-buttonTogglesLed.py: when the button is pressed, toggle the led
+# ------------------------------------------------------------------
+#
+import mraa
+import time
+
+LOW = 0
+HIGH = 1
+
+digitalInPin = 8
+digitalInGpio = mraa.Gpio( digitalInPin )
+
+ledOutPin = 3
+ledOutGpio = mraa.Gpio( ledOutPin )
+ledOutGpio.dir(mraa.DIR_OUT)
+
+#############################
+# Functions
+#
+def toggleLedState( ledOutState ) :
+	print ( 'toggleLedState ledOutState = ' + str(ledOutState) )
+	if ( ledOutState == LOW ) :
+		 ledOutState = HIGH
+	else :
+		 ledOutState = LOW
+	ledOutGpio.write( ledOutState )
+	print ( 'toggleLedState returning ledOutState = ' + str(ledOutState) )
+	return ledOutState
+
+#
+# mainline loop:
+#
+ledOutState = LOW
+ledOutGpio.write( ledOutState )
+loopDelaySecs = 0.1
+afterToggleDelaySecs = 3
+
+while True:
+	digitalInState = digitalInGpio.read()
+	print( 'digitalInState: ' + str(digitalInState) )
+
+	if( digitalInState == 1 ) :
+		ledOutState = toggleLedState( ledOutState )
+		time.sleep( afterToggleDelaySecs )
+
+	time.sleep( loopDelaySecs )
+
+exit(0)
