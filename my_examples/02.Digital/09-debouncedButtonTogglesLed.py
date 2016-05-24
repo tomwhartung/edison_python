d05fb7fdd95d830c8f4ad077a48566f5970d534b
Tom W Hartung <tomwhartung@gmail.com>
2016-05-24 17:57:27 -0600

    Another fix.

--- my_examples/02.Digital/09-debouncedButtonTogglesLed.py
+++ my_examples/02.Digital/09-debouncedButtonTogglesLed.py
@@ -57,7 +57,7 @@ def loop() :
 	global ledOutState
 	global lastDebounceTime
 	currentInState = digitalInGpio.read()
-	print( 'digitalInState: ' + str(digitalInState) )
+	print( currentlInState: ' + str(currentInState) )
 
 	currentDatetime = datetime.datetime.today()
 	lastDebounceTime = currentDatetime
